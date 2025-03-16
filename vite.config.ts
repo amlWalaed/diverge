9711fd93467c456d81c4551f1ef993464cd66c6a
Aml walaed <amlwalaed@gmail.com>
2025-03-16 17:07:08 +0200

    Update index.html title and add Tailwind CSS; remove base.css and integrate Tailwind in main.css

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,13 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite'
 import RekaResolver from 'reka-ui/resolver'
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss(),
     Components({
       resolvers: [RekaResolver()],
     }),
